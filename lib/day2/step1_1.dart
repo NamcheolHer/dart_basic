9375872c634509caf452d9734745640d1ca50688
namcheolher <nc.her@modulabs.co.kr>
2023-11-28 08:15:42 +0900

    day2.1

--- lib/day2/step1_1.dart
+++ lib/day2/step1_1.dart
@@ -1,4 +1,15 @@
+import 'dart:math';
+
+
 void main() {
   // main 함수는 dart의 진입점이다.
   print("this is dart main fucntion");
+  // 정수형 변수
+  int number = 42;
+
+  //실수형 변수
+  double pi = 3.14;
+
+  //
+
 }
